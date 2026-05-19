4257714271b69e350b2cfffd659b5095c97ba9be
Clananas <clemens.dieterich@icloud.com>
2026-05-19 21:21:51 +0200

    hjkbkghu

--- convo.py
+++ convo.py
@@ -1,23 +1,72 @@
+import math
+
 from PIL import Image
 import numpy as np
 
+from morphological import show_image
+
 
 def make_kernel(ksize, sigma):
-    return  # implement the Gaussian kernel here
+    # implement the Gaussian kernel here
+    r = ksize // 2
+    """
+    kernel = np.zeros((ksize, ksize))
+    for i in range(ksize):
+        for j in range(ksize):
+            x = i - ksize
+            y = j - ksize
+            kernel[i, j] = np.exp(-(x**2 + y**2) / (2 * sigma**2))
+
+    kernel = kernel / np.sum(kernel)
+    """
+    ###############################################################
+
+    y, x = np.mgrid[-r : r + 1, -r : r + 1]
+
+    kernel = np.exp(x**2 + y**2 / (2 * sigma**2))
+
+    kernel = kernel / np.sum(kernel)
+
+
+    return kernel
+
+
 
 
 def slow_convolve(arr, k):
-    return  # implement the convolution with padding here
+
+    out_img = np.zeros_like(arr, dtype=float)
+
+    ksize = k.shape[0]
+    height, width, channel = arr.shape
+    r = ksize // 2
+
+    pad = ksize // 2
+    padded = np.pad(arr, pad_width=pad, mode='constant', constant_values=0)
+
+
+    for i in range(width):
+        for j in range(height):
+            for c in range(channel):
+                value = 0
+                for u in range(ksize):
+                    for v in range(ksize):
+                            value += k[u, v] * padded[i + u, j + v, c]
+                out_img[i, j, c] = value
+
+    return out_img
 
 
 if __name__ == '__main__':
-    k = make_kernel(3, 1)   # todo: find better parameters
+    k = make_kernel(30, 1)  # todo: find better parameters original: ksize 3, sigma 1
     
     # TODO: chose the image you prefer
-    # im = np.array(Image.open('input1.jpg'))
-    # im = np.array(Image.open('input2.jpg'))
-    # im = np.array(Image.open('input3.jpg'))
-    
+    # im = np.array(Image.open('data/input1.jpg'))
+    # im = np.array(Image.open('data/input2.jpg'))
+    im = np.array(Image.open('data/input3.jpg'))
+    o_img = slow_convolve(im, k)
+    show_image(o_img)
+
     # TODO: blur the image, subtract the result to the input,
     #       add the result to the input, clip the values to the
     #       range [0,255] (remember warme-up exercise?), convert
